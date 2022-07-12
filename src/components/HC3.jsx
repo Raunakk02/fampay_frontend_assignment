8db53edace394090fa216f580ee0bf0715b23ccb
raunak_k02 <raunakk728@gmail.com>
2022-07-12 12:51:22 +0530

    feat: added HC3 contextual card

--- src/components/HC3.jsx
+++ src/components/HC3.jsx
@@ -1,5 +1,13 @@
 function HC3() {
-	return <div className="card hc3">hc3</div>;
+	return (
+		<div className="card hc3">
+			<h3>Big Display Card with Action</h3>
+			<h5>
+				This is a sample text for subtitle that you can add to contextual cards
+			</h5>
+			<button className="cta">Action</button>
+		</div>
+	);
 }
 
 export default HC3;
