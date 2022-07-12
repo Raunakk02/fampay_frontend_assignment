b6e8c533acaae39dbf78322cf65c9c81a3df2fea
raunak_k02 <raunakk728@gmail.com>
2022-07-12 14:50:39 +0530

    feat: added HC9 contextual card

---
+++ src/components/HC9.jsx
@@ -0,0 +1,5 @@
+function HC9() {
+	return <div className="card hc9"></div>;
+}
+
+export default HC9;
--- src/components/ScrollContainer.jsx
+++ src/components/ScrollContainer.jsx
@@ -1,6 +1,7 @@
 import HC3 from "./HC3";
 import HC5 from "./HC5";
 import HC6 from "./HC6";
+import HC9 from "./HC9";
 
 function ScrollContainer() {
 	return (
@@ -8,6 +9,7 @@ function ScrollContainer() {
 			<HC3></HC3>
 			<HC6></HC6>
 			<HC5></HC5>
+			<HC9></HC9>
 		</div>
 	);
 }
